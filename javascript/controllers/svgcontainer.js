c8263d30b7cedb06ced329f3d51a5e7cbaca8573
Diederik Mathijs <diederik.mathijs@gmail.com>
2019-11-12 13:54:04 +0100

    Added quick margin scetch

--- javascript/controllers/svgcontainer.js
+++ javascript/controllers/svgcontainer.js
@@ -6,15 +6,16 @@ export default function SVGContainer(renderWindow, xmlParser, pageSizing){
     this.width = renderWindow.clientWidth
     this.pageSizing = pageSizing
     this.xmlParser = xmlParser
-   
+
     // Keep track of the real ratio (sized to DOM)
     this.ratio = CalculateDomRatio(renderWindow, pageSizing)
     this.height = CalculateAndSetWindowHeight(renderWindow, pageSizing, this.ratio)
    
+    // Keep track of position of measures
+    this.margins = CalculateMeasureMargins(renderWindow, pageSizing["page-margins"][0], this.ratio)
 
-    this.margins = pageSizing["page-margins"][0]
-
-    this.Data = {};
+    // Initialize SVG
+    this.svgObject = {"svg":{"_width":this.width, "_height":this.height,"g":[]}};
 
     this.Generate = Generate;
     this.setRenderWindowHeight = setRenderWindowHeight
@@ -26,6 +27,16 @@ function CalculateAndSetWindowHeight(renderWindow, pageSizing, ratio){
     setRenderWindowHeight(renderWindow, _height)
     
     return _height;
+}
+
+// Re-calculates the margins using the ratio of the viewPort
+function CalculateMeasureMargins(renderWindow, margins, ratio){
+    return{
+        "leftMargin":margins["left-margin"]*ratio,
+        "rightMargin":margins["right-margin"]*ratio,
+        "topMargin":margins["top-margin"]*ratio,
+        "bottomMargin":margins["bottom-margin"]*ratio
+    }
 }
 
 function CalculateDomRatio(renderWindow, pageSizing){
@@ -55,7 +66,29 @@ function Generate(){
     //     }
     // }
     
-    // return this.xmlParser.ParseToXml(example_obj)
+    
+    return this.xmlParser.ParseToXml(_drawMargins(this.svgObject, this.margins, this.width, this.height))
 }
 
+
+function _drawMargins(svgObject, margins, width, height){
+
+    _drawLine(svgObject, margins.leftMargin, margins.topMargin, width - margins.rightMargin, margins.topMargin)
+    _drawLine(svgObject, margins.leftMargin, margins.topMargin, margins.leftMargin, height-margins.bottomMargin)
+    _drawLine(svgObject, margins.leftMargin, height-margins.bottomMargin, width - margins.rightMargin, height-margins.bottomMargin)
+    _drawLine(svgObject, width - margins.rightMargin, margins.topMargin, width - margins.rightMargin, height-margins.bottomMargin)
+
+    return svgObject
+}
+
+
+function _drawLine(svgObject, x1, y1, x2, y2){
+    svgObject.svg.g.push({"path":
+    {"_d":`M${x1},${y1} L${x2} ${y2}`,
+    "_stroke-width":"1",
+    "_stroke":"red",
+    "_fill":"none",
+    }});
+    return svgObject;
+}
 
